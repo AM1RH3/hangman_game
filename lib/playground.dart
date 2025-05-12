bee765c329e564efe34a781a9e18da3ea743ba3f
AM1RH3 <amirhossein.rasooli@outlook.com>
2025-05-12 18:08:39 +0330

    created flutter

---
+++ lib/playground.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Playground extends StatelessWidget {
+  const Playground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: const Text("Hangman Game")));
+  }
+}
